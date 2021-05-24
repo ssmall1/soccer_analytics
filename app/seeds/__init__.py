9a28655473f3515ce493238eac6240b5fc625fd8
schuler <schuler.small.10@gmail.com>
2021-05-23 19:33:28 -0500

    created event migration, fixed errors in data

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .events import seed_events, undo_events
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -9,10 +10,14 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_events()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_events()
     # Add other undo functions here
+
+
---
+++ app/seeds/events.py
@@ -0,0 +1,25 @@
+from app.models import db, Event
+
+def seed_events():
+
+    test = Event(eventId_x=8, subEventName='Simple pass',
+                tags=[{'id': 1801}], playerId=25413, 
+                positions=[{'y': 49, 'x': 49}, {'y': 78, 'x': 31}],
+                matchId=2499719, eventName="Pass", teamId=1609,
+                matchPeriod="1H", eventSec=2.7586489999999912,
+                gameWeek=1, date="2017-08-11 18:45:00,2499719",
+                label="Arsenal - Leicester City, 4 - 3",
+                firstName="Alexandre", middleName="",
+                lastName="Lacazette")
+
+    db.session.add(test)
+
+    db.session.commit()
+
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and resets
+# the auto incrementing primary key
+def undo_events():
+    db.session.execute('TRUNCATE events RESTART IDENTITY CASCADE;')
+    db.session.commit()
