351b777d579cb913cf809c830a98104597b3b4a2
schuler <schuler.small.10@gmail.com>
2021-06-03 11:47:02 -0500

    imported logo and favicon

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { useSelector, useDispatch } from "react-redux";
-// import { Redirect } from "react-router-dom";
+import { NavLink, Redirect } from "react-router-dom";
 // import { login } from "../../store/session";
 // import pitchImg from "../../resources/pitch.jpg";
 import './Splash.css';
@@ -8,9 +8,16 @@ import './Splash.css';
 const Splash = () => {
 
     return(
-        <div>
-            What it dooooo
-        </div>
+        <>
+            <div>
+                What it dooooo
+            </div>
+
+            <div className="splash-auth-links">
+                <NavLink to="/sign-in">Sign-in</NavLink>
+                <NavLink to="/sign-up">Sign-up</NavLink>
+            </div>
+        </>
     )
 }
 
