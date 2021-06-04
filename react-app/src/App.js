d565bc1d7cd20384bae5fb117f30d3d534112704
schuler <schuler.small.10@gmail.com>
2021-06-04 12:38:22 -0500

    created user profile component and conditional rendering of navbar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,7 +6,7 @@ import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
-import User from "./components/User";
+import UserProfile from "./components/UserProfile";
 import Splash from "./components/Splash";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -28,9 +28,23 @@ function App() {
     return null;
   }
 
+  let navComponent;
+
+  if (user != null) {
+    navComponent = (
+      <NavBar />
+    )
+  } else {
+    navComponent = (
+      <>
+      </>
+    )
+  }
+
+
   return (
     <BrowserRouter>
-      {/* <NavBar /> */}
+      {navComponent}
       <Switch>
         <Route path="/welcome" exact={true}>
           <Splash />
@@ -45,7 +59,7 @@ function App() {
           <UsersList/>
         </ProtectedRoute>
         <ProtectedRoute path="/users/:userId" exact={true} >
-          <User />
+          <UserProfile />
         </ProtectedRoute>
         <ProtectedRoute path="/" exact={true} >
           <Home />
