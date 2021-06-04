018e7d213c1d6d225522173872c14253fd787029
schuler <schuler.small.10@gmail.com>
2021-06-04 14:40:42 -0500

    working on navbar

---
+++ react-app/src/components/NavBar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import LogoutButton from '../auth/LogoutButton';
+import './NavBar.css';
+import logo from "../../resources/logo.png";
+
+const NavBar = () => {
+  const user = useSelector(state => state.session.user);
+
+  return (
+    <div className="nav-wrapper">
+        <div className="nav-home-container">
+          <NavLink to="/" exact={true} activeClassName="active">
+            <img id="nav-logo" src={logo}></img>
+          </NavLink>
+        </div>
+        <div className="nav-user-links">
+          <div>
+            <NavLink to={`/users/${user.id}`} exact={true} activeClassName="active">
+              Profile
+            </NavLink>
+          </div>
+          {/* <li>
+            <NavLink to="/users" exact={true} activeClassName="active">
+              Users
+            </NavLink>
+          </li> */}
+          <div>
+            <LogoutButton />
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default NavBar;
