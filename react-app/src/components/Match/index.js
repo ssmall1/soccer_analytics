3c12da742e66816358f60cbab7c83234d27a321f
schuler <schuler.small.10@gmail.com>
2021-06-08 15:22:59 -0500

    working on adding to favorites, store created

--- react-app/src/components/Match/index.js
+++ react-app/src/components/Match/index.js
@@ -3,33 +3,48 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import * as eventsReducer from "../../store/events";
 import * as matchesReducer from "../../store/matches";
+import * as favoriteReducer from "../../store/favorite";
 import EventChart from "../EventChart";
 import "./Match.css";
 
 const Match = () => {
     const matchKeyStr = useParams();
-    const matchKey = parseInt(matchKeyStr.matchKey, 10)
+    const matchKey = parseInt(matchKeyStr.matchKey, 10);
     const dispatch = useDispatch();
-    const events = useSelector(state => state.events.events)
-    const match = useSelector(state => state.matches.match)
-    console.log(match)
+    const events = useSelector(state => state.events.events);
+    const match = useSelector(state => state.matches.match);
+    const user = useSelector(state => state.session.user);
     
     useEffect(() => {
-        dispatch(eventsReducer.getMatchEvents(matchKey))
-        dispatch(matchesReducer.getMatch(matchKey))
+        dispatch(eventsReducer.getMatchEvents(matchKey));
+        dispatch(matchesReducer.getMatch(matchKey));
     }, [])
 
     useEffect(() => {
-        dispatch(eventsReducer.getEvents())
+        dispatch(eventsReducer.getEvents());
     }, [])
 
     if (!events) return null;
 
     if(!match) return null;
 
+    async function handleFavorite() {
+        const userId = user.id;
+        const matchId = matchKey;
+        const payload = {
+            userId,
+            matchId
+        }
+
+        await dispatch(favoriteReducer.postFavorite(payload));
+        // if favorite exists dispatch remove
+        // if favorite doesn't exist dispatch add
+    }
+
     return(
         <div className="match-wrapper">
             <div className="match-info">
+                <button id="match-favorite-btn" onClick={handleFavorite}>Favorite</button>
                 <div id="match-page-title">{match.match_name}</div>
                 <img id="match-page-img" src={match.match_img}/>
             </div>
---
+++ react-app/src/store/favorite.js
@@ -0,0 +1,41 @@
+const POST_FAVORITE = "favorites/POST_FAVORITE";
+
+const setFavorite = (match) => {
+    return{
+        type: POST_FAVORITE,
+        match
+    }
+}
+
+export const postFavorite = (payload) => async (dispatch) => {
+    const response = await fetch(`/api/favorites/${payload.userId}`, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    if (response.ok) {
+        const favorite = await response.json();
+        dispatch(setFavorite(favorite))
+        return favorite
+    }
+}
+
+
+const initialState = {};
+
+const favoriteReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case POST_FAVORITE:
+            newState = { ...state }
+            newState.favorites = [...state.favorites, action.favorite]
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default favoriteReducer;
