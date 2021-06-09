71f607b04eb2da11be9d0179b0e069fc6442fc11
schuler <schuler.small.10@gmail.com>
2021-06-09 10:19:19 -0500

    added bg color to favorite card and about links to footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,6 +5,12 @@ const Footer = () => {
 
     return(
         <div className="footer-wrapper">
+            <a href="https://github.com/ssmall1/soccer_analytics" rel="noopener noreferrer" target="_blank">
+                Github |
+            </a>
+            <a href="https://www.linkedin.com/in/schuler-small/" rel="noopener noreferrer" target="_blank">
+                Connect |
+            </a>
             <i className="fa fa-copyright" aria-hidden="true"></i>
             Schuler Small | All Rights Reserved
         </div>
