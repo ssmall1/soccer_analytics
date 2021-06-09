7691dcf0df85e65d4def763f2fed05e97aea64e8
schuler <schuler.small.10@gmail.com>
2021-06-09 11:11:02 -0500

    more footer and scroll styling

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,10 +6,10 @@ const Footer = () => {
     return(
         <div className="footer-wrapper">
             <a href="https://github.com/ssmall1/soccer_analytics" rel="noopener noreferrer" target="_blank">
-                Github |
+                Github  |
             </a>
             <a href="https://www.linkedin.com/in/schuler-small/" rel="noopener noreferrer" target="_blank">
-                Connect |
+                Connect  |
             </a>
             <i className="fa fa-copyright" aria-hidden="true"></i>
             Schuler Small | All Rights Reserved
