b46fd38d09cb5eea894b83bfe1c06174ff3402c0
ssmall1 <ssmall@appacademy.io>
2021-08-24 22:48:01 -0500

    match page formatting

--- react-app/src/components/Match/index.js
+++ react-app/src/components/Match/index.js
@@ -64,6 +64,23 @@ const Match = () => {
                     : <button id="match-favorite-btn" onClick={handleFavorite}>Favorite</button> 
                 }
                 <div id="match-page-title">{match.match_name}</div>
+                {/* <div className="chart-selector-wrapper">
+                    <span>Filter By:</span>
+                    <select onChange={(e) => setEventType(e.target.value)} className="event-type-selector">
+                        <option value="Shot">Shots</option>
+                        <option value="Pass">Passes</option>
+                        <option value="Free Kick">Free Kicks</option>
+                        <option value="Duel">Duels</option>
+                        <option value="Foul">Fouls</option>
+                        <option value="Save attempt">Saves</option>
+                    </select>
+                </div> */}
+                <img id="match-page-img" src={match.match_img} alt={`${match.match_name}`}/>
+            </div>
+            <div className="match-content-wrapper">
+                <div className="pitch-chart-container">
+                    <EventScatterChart id="pitch-chart" matchKey={matchKey} eventType={eventType}/>
+                </div>
                 <div className="chart-selector-wrapper">
                     <span>Filter By:</span>
                     <select onChange={(e) => setEventType(e.target.value)} className="event-type-selector">
@@ -74,12 +91,6 @@ const Match = () => {
                         <option value="Foul">Fouls</option>
                         <option value="Save attempt">Saves</option>
                     </select>
-            </div>
-                <img id="match-page-img" src={match.match_img} alt={`${match.match_name}`}/>
-            </div>
-            <div className="match-content-wrapper">
-                <div className="pitch-chart-container">
-                    <EventScatterChart id="pitch-chart" matchKey={matchKey} eventType={eventType}/>
                 </div>
                 <Comments id="comments-component"/>
             </div>
